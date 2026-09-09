8cd533a0939eceba2a22aaf8cbf797e3cbeb6003
Patricia-P-G <patriciapuienaru@gmail.com>
2026-09-09 18:35:25 +0200

    font updated

--- wp-content/themes/nordicbloom/functions.php
+++ wp-content/themes/nordicbloom/functions.php
@@ -24,6 +24,13 @@ function nordicbloom_enqueue_styles()
         get_stylesheet_uri()
     );
 
+    // Google Font - Inter
+    wp_enqueue_style(
+    'nordicbloom-google-fonts',
+    'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
+    []
+    );
+
     // Global / base styles
     wp_enqueue_style(
         'nordicbloom-base',
